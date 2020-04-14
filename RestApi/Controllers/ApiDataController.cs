d350c2a46e3d4bcb9029f82e783ac95648960506
talpro6370 <45435657+talpro6370@users.noreply.github.com>
2020-04-14 15:46:49 +0300

    Add files via upload

---
+++ RestApi/Controllers/ApiDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestApiData.Controllers
+{
+    public class ApiDataController : Controller
+    {
+        // GET: ApiData
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestApi/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tutor_Database.RestApiHelper;
+
+namespace RestApiData.Controllers
+{
+    public class DataController : ApiController
+    {
+        
+        [HttpGet]
+        [Route("api/DataController/GetProfessions")]
+        public IHttpActionResult GetProfessions()
+        {
+            RestApiAllData rest = new RestApiAllData();
+            return Ok(rest.GetProfessions());
+        }
+
+        [HttpGet]
+        [Route("api/DataController/GetCities")]
+        public IHttpActionResult GetCities()
+        {
+            RestApiAllData rest = new RestApiAllData();
+            return Ok(rest.GetCities());
+        }
+    }
+}
