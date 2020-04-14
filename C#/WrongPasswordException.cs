1b2428a866314570bbe70b44d1c254907dda6893
talpro6370 <45435657+talpro6370@users.noreply.github.com>
2020-04-14 15:44:21 +0300

    Add files via upload

---
+++ C#/WrongPasswordException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor_Database.Exceptions
+{
+    [Serializable]
+    internal class WrongPasswordException : ApplicationException
+    {
+        public WrongPasswordException()
+        {
+        }
+
+        public WrongPasswordException(string message) : base(message)
+        {
+
+        }
+
+        public WrongPasswordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected WrongPasswordException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
