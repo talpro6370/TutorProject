28cba3d9d71244f54eeffc572e4b698cffb02836
talpro6370 <45435657+talpro6370@users.noreply.github.com>
2020-04-14 15:53:34 +0300

    Add files via upload

---
+++ Web/Scripts/Customize/Registration-Controller.js
@@ -0,0 +1,93 @@
+﻿angular.module("tutor_app", []).controller("Registration-Controller", function ($scope, $http) {
+    $scope.first_name = null;
+    $scope.last_name = null;
+    $scope.user_name = null;
+    $scope.password = null;
+    $scope.email = null;
+    $scope.phone_number = null;
+    $scope.city = null;
+    $scope.profession = null;
+    $scope.gender = null;
+
+    $scope.Post_data = function (first_name, last_name, user_name, password, email, phone_number,city, profession,gender) {
+        var data = {
+            first_name: first_name,
+            last_name: last_name,
+            user_name: user_name,
+            password: password,
+            email: email,
+            phone_number: phone_number,
+            city:city,
+            profession: profession,
+            gender:gender
+        };
+        $scope.onSubmit = function () {
+            $http.post("/api/TutorRegistrationCtrl/SubmitData", JSON.stringify(data)).then(function (response) {
+                var dataVal = {
+                    UserName: $('#user_name').val().trim(),
+                    Password: $('#password').val().trim(),
+                    ConfirmPassword: $('#password').val().trim()
+                }
+                if (response.data) {
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        width: '300px',
+                        background: '#21dc35',
+                        onOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    })
+                    Toast.fire({
+                        icon: 'success',
+                        customClass: 'customize',
+                        title: 'Register successfully!'
+                        
+                    }).then(function () {
+                        $.ajax({
+                            url: "/api/Account/Register",
+                            type: 'POST',
+                            data: dataVal,
+                            success: function () {
+                                console.log("Auth completed!");
+                                window.location.href ='http://localhost:49294/Pages/TutorLoginPage';
+                            }, //Continue with getting the access-token from generator. Use dotnettricks website
+                            error: function () {
+                                console.log("Auth failed!");
+                            }
+                        })
+                    })
+                }
+                
+                else {
+                    swal.fire({
+                        icon: 'warning',
+                        title: 'Username is already exist!',
+                        text: 'Please try again',
+                        timerProgressBar: true,
+                        allowOutsideClick: false,
+                        timer: 3000
+                    });
+                    setTimeout(() => {
+                        location.reload();
+                    }, 3000);
+                }
+        }
+        )
+        }
+    }
+   
+    $scope.cities = [];
+    $http.get('/api/DataController/GetCities').then(function (response) {
+        $scope.cities = response.data;
+    });
+    $scope.profs = [];
+    $http.get('/api/DataController/GetProfessions').then(function (response) {
+        $scope.profs = response.data;
+    });
+ 
+})
---
+++ Web/Scripts/Customize/TutorLoginController.js
@@ -0,0 +1,81 @@
+﻿angular.module("tutor_app", []).controller("Login-Controller", function ($scope, $http) {
+    $scope.username = null;
+    $scope.password = null;
+    $scope.Post_data = function (username,password) {
+        var data = {
+            username: username,
+            password: password
+        };
+    
+        $scope.onSubmit = function () {
+        $http.post("/api/TutorLoginCtrl/LoginData", JSON.stringify(data)).then(function (response) {
+            var dataFromCtrl = response.data;
+            if (response.data) {
+                $.ajax({
+                    url: "/Token",
+                    type: "POST",
+                    contentType: 'application/json',
+                    data: {
+                        username: data.username,
+                        password: data.password,
+                        grant_type: 'password'
+                    },
+                    
+                    success: function (response) {
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 2000,
+                            timerProgressBar: true,
+                            width: '300px',
+                            background: '#21dc35',
+                            onOpen: (toast) => {
+                                toast.addEventListener('mouseenter', Swal.stopTimer)
+                                toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                        })
+                        sessionStorage.setItem('userName', response.userName);
+                        sessionStorage.setItem('accessToken', response.access_token);
+                        if (dataFromCtrl != null) {
+                            Toast.fire({
+                                icon: 'success',
+                                customClass: 'customize',
+                                title: 'Register successfully!'
+                            });
+                            window.href = "http://localhost:49294/Pages/TutorHomePage";
+                        }  
+                         else if (dataFromCtrl == null) {
+                            swal.fire({
+                                icon: 'warning',
+                                title: 'Unexpected token ',
+                                text: 'Please try again',
+                                timerProgressBar: true,
+                                allowOutsideClick: false,
+                                timer: 3000
+                            }).then(function() {
+                                window.location.reload();
+                            })
+                                
+                        }     
+                    },
+                    error: function () {
+                        swal.fire({
+                            icon: 'warning',
+                            title: 'Unexpected token ',
+                            text: 'Please try again',
+                            timerProgressBar: true,
+                            allowOutsideClick: false,
+                            timer: 3000
+                        });
+                    }
+                })
+            }
+        })
+        }
+        $scope.Logout = function () {
+            sessionStorage.clear();
+            Location.href("");
+        }
+    }
+})
