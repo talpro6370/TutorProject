9edd2072cf75f1b5fffc57a262902d4af4421f40
talpro6370 <45435657+talpro6370@users.noreply.github.com>
2020-04-14 15:50:50 +0300

    Add files via upload

---
+++ Web/Models/StudentDets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class StudentDets
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string user_name { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string city { get; set; }
+        public int age { get; set; }
+        public long phone_number { get; set; }
+
+    }
+}
